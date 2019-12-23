6a930e05e54c2db10ca4779b459907964d661711
b08902043 <b08902043@ntu.edu.tw>
2019-12-23 06:53:59 -0500

    add main.py and modify arg.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from crawler import Crawler
+from args import get_args
+
+
+if __name__ == '__main__':
+    args = get_args()  
+    crawler = Crawler()
+    contents = crawler.crawl(args.start_date, args.end_date)
+    # TODO: write content to file according to spec
+    with open(args.output,'w') as f:
+        for date, title,content in contents:
+            out_str = f'{str(date)},"{title}","{content}"\n'
+            #print(out_str)
+            f.write(out_str)
